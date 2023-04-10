2ebffc86d6f5d12244bf33c47a3d29472054500a
Crawford H <crawf.h2@gmail.com>
2023-04-10 17:39:13 -0400

    Fixed acceleration physics

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,8 @@ use coffee::graphics::{Window, Color, Frame, Batch, Image, Sprite, Rectangle, Ve
 use coffee::input::{KeyboardAndMouse, mouse, keyboard};
 use rayon::prelude::*;
 
+use rand::Rng;
+
 use crate::particle::Particle;
 use crate::config::Config;
 
@@ -15,6 +17,8 @@ pub struct Application {
     camera_transform: Transformation,
     zoom: f32,
     camera_position: Point,
+    scale: f32,
+    time_scale: f32,
 
     // config to store various constants
     config: Config, 
@@ -24,28 +28,126 @@ impl Application {
     fn update_acceleration_par(&mut self) {
         let entities_clone = self.entities.clone();
         self.entities.par_iter_mut().for_each(move |particle| {
-            entities_clone.iter().for_each(|other_particle| {
-                particle.calculate_acceleration(other_particle)
-            });
+            particle.acceleration.x = 0.;
+            particle.acceleration.y = 0.;
+            entities_clone.iter().for_each(|other| { particle.calculate_acceleration(other) });
         })
     }
 
     fn _update_acceleration_series(&mut self) {
         let entities_clone = self.entities.clone();
-        self.entities.iter_mut().for_each(move |particle| {
-            entities_clone.iter().for_each(|other_particle| {
-                particle.calculate_acceleration(other_particle)
-            });
-        })
+        for particle in self.entities.iter_mut() {
+            for other in entities_clone.iter() {
+                particle.calculate_acceleration(other)
+            };
+        };
+    }
+
+    fn generate_random_particles(&mut self, num_particles: i32) {
+        let generator = &mut rand::thread_rng();
+        for _ in 0..num_particles {
+            self.entities.push(Particle { 
+                velocity: Vector::new(generator.gen_range(-5.0e-1..5.0e-1), generator.gen_range(-5.0e-1..5.0e-1)), 
+                position: Point::new(generator.gen_range(-3.0e3..3.0e3), generator.gen_range(-3.0e3..3.0e3)), 
+                mass: generator.gen_range(10.0e2..1.0e6), 
+                acceleration: Vector::new(0., 0.),
+                id: self.entities.len() as u16, 
+            })
+        }
+    }
+
+    fn generate_solar_system(&mut self) {
+        self.scale = 1500. / 4495.060e9;
+        self.time_scale = 100000.;
+        // generate the sun
+        self.entities.push(Particle { 
+            id: self.entities.len() as u16, 
+            velocity: Vector::new(0., 0.), 
+            position: Point::new(0., 0.), 
+            mass: 1.989e30, 
+            acceleration: Vector::new(0., 0.) 
+        });
+
+        // mercury
+        self.entities.push(Particle { 
+            id: self.entities.len() as u16, 
+            velocity: Vector::new(0., 47.36e3), 
+            position: Point::new(57.909e9, 0.), 
+            mass: 0.33011e24, 
+            acceleration: Vector::new(0., 0.) 
+        });
+
+        // venus
+        self.entities.push(Particle { 
+            id: self.entities.len() as u16, 
+            velocity: Vector::new(0., 35.02e3), 
+            position: Point::new(108.209e9, 0.), 
+            mass: 4.8675e24, 
+            acceleration: Vector::new(0., 0.) 
+        });
+
+        // earth
+        self.entities.push(Particle { 
+            id: self.entities.len() as u16, 
+            velocity: Vector::new(0., 29.78e3), 
+            position: Point::new(149.596e9, 0.), 
+            mass: 5.9724e24, 
+            acceleration: Vector::new(0., 0.) 
+        });
+
+        // mars
+        self.entities.push(Particle { 
+            id: self.entities.len() as u16, 
+            velocity: Vector::new(0., 24.07e3), 
+            position: Point::new(227.923e9, 0.), 
+            mass: 0.64171e24, 
+            acceleration: Vector::new(0., 0.) 
+        });
+
+        // jupiter
+        self.entities.push(Particle { 
+            id: self.entities.len() as u16, 
+            velocity: Vector::new(0., 13e3), 
+            position: Point::new(778.57e9, 0.), 
+            mass: 1898.19e24, 
+            acceleration: Vector::new(0., 0.) 
+        });
+
+        // saturn
+        self.entities.push(Particle { 
+            id: self.entities.len() as u16, 
+            velocity: Vector::new(0., 9.68e3), 
+            position: Point::new(1433.529e9, 0.), 
+            mass: 568.34e24, 
+            acceleration: Vector::new(0., 0.) 
+        });
+
+        // uranus
+        self.entities.push(Particle { 
+            id: self.entities.len() as u16, 
+            velocity: Vector::new(0., 6.80e3), 
+            position: Point::new(2872.463e9, 0.), 
+            mass: 86.813e24, 
+            acceleration: Vector::new(0., 0.) 
+        });
+
+        // neptune
+        self.entities.push(Particle { 
+            id: self.entities.len() as u16, 
+            velocity: Vector::new(0., 5.43e3), 
+            position: Point::new(4495.060e9, 0.), 
+            mass: 102.413e24, 
+            acceleration: Vector::new(0., 0.) 
+        });
     }
 }
 
 impl Game for Application {
-    type Input = KeyboardAndMouse; // No input data
+    type Input = KeyboardAndMouse;
     type LoadingScreen = ();
 
-    const TICKS_PER_SECOND: u16 = 30;
-
+    const TICKS_PER_SECOND: u16 = 64;
+    const DEBUG_KEY: Option<keyboard::KeyCode> = Some(keyboard::KeyCode::F12);
 
     fn load(_window: &Window) -> Task<Application> {
         let config = Config::new();
@@ -58,52 +160,61 @@ impl Game for Application {
                 zoom: 1.,
                 camera_position: Point::new(0.0, 0.0),
                 camera_transform: Transformation::identity(),
+                scale: 1.,
+                time_scale: 1.,
         })
     }
 
-    fn update(&mut self, _window: &Window) {
-        Self::update_acceleration_par(self);
-    }
 
     fn draw(&mut self, frame: &mut Frame, timer: &Timer) {
-        // Clear the current frame
         frame.clear(Color::BLACK);
+        
         let mut target = frame.as_target();
         let mut camera = target.transform(self.camera_transform);
-
+        
+        Self::update_acceleration_par(self);
         self.entities.par_iter_mut().for_each(|particle| { 
-            particle.velocity += particle.acceleration * timer.next_tick_proximity();
-            particle.position += particle.velocity * timer.next_tick_proximity();
+            particle.velocity += particle.acceleration * timer.next_tick_proximity() * self.time_scale;
+            particle.position += particle.velocity * timer.next_tick_proximity() * self.time_scale;
         });
 
         let sprite_offset = Vector::new(self.config.sprite_width as f32 * self.config.sprite_scale / 2., self.config.sprite_height as f32 * self.config.sprite_scale / 2.);
-        let sprites = self.entities.iter().map(|particle| {
+        let sprites = self.entities.par_iter().map(|particle| {
+            // println!("Position: {} Screen Position: {} Veclocity: {}", particle.position, particle.position * self.scale, particle.velocity);
             Sprite {
                 source: self.particle_sprite_quad,
-                position: particle.position - sprite_offset,
+                position: (particle.position * self.scale) - sprite_offset,
                 scale: (self.config.sprite_scale, self.config.sprite_scale),
             }
         });
     
         self.batch.clear();
-        self.batch.extend(sprites);
+        self.batch.par_extend(sprites);
         self.batch.draw(&mut camera);
     }
 
     fn interact(&mut self, input: &mut Self::Input, _window: &mut Window) {
+        let cursor_position = input.mouse().cursor_position();
+        let x_position = (cursor_position.x - self.camera_position.x) / self.scale;
+        let y_position = (cursor_position.y - self.camera_position.y) / self.scale;
         if input.mouse().is_button_pressed(mouse::Button::Left) {
-            let cursor_position = input.mouse().cursor_position();
-            self.entities.push(Particle::new(cursor_position.x - self.camera_position.x, cursor_position.y - self.camera_position.y, 1000.));
+            self.entities.push(Particle::new(x_position, y_position, 1000., self.entities.len() as u16));
         }
 
         if input.keyboard().was_key_released(keyboard::KeyCode::Key1) {
-            let cursor_position = input.mouse().cursor_position();
-            self.entities.push(Particle::new(cursor_position.x - self.camera_position.x, cursor_position.y - self.camera_position.y, 1.0e6));
+            self.entities.push(Particle::new(x_position, y_position, 1.0e6, self.entities.len() as u16));
         }
 
         if input.keyboard().was_key_released(keyboard::KeyCode::Key2) {
-            let cursor_position = input.mouse().cursor_position();
-            self.entities.push(Particle::new(cursor_position.x - self.camera_position.x, cursor_position.y - self.camera_position.y, 1.0e8));
+            self.entities.push(Particle::new(x_position, y_position, 1.0e10, self.entities.len() as u16));
+        }
+
+        if input.keyboard().was_key_released(keyboard::KeyCode::Key3) {
+            Self::generate_random_particles(self, 4000);
+        }
+
+        if input.keyboard().was_key_released(keyboard::KeyCode::Key4) {
+            Self::generate_solar_system(self);
         }
 
         if input.keyboard().is_key_pressed(keyboard::KeyCode::W) {
@@ -119,17 +230,14 @@ impl Game for Application {
             self.camera_position.x -= 5.;
         }
         if input.keyboard().was_key_released(keyboard::KeyCode::R) {
+            self.time_scale = 1.;
+            self.scale = 1.;
             self.entities = Vec::new();
         }
         if input.keyboard().was_key_released(keyboard::KeyCode::P) {
             println!("Number of particles = {}", self.entities.len());
         }
 
-        // let scroll_wheel = input.mouse().wheel_movement().vertical;
-        // let _cursor_position = input.mouse().cursor_position();
-        // if scroll_wheel != 0. {
-        //     self.zoom += scroll_wheel * 0.05;
-        // }
         self.camera_transform = Transformation::scale(self.zoom) * Transformation::translate(
             Vector::new(self.camera_position.x, self.camera_position.y));
     }
--- src/particle.rs
+++ src/particle.rs
@@ -3,10 +3,10 @@ use coffee::graphics::{Vector, Point};
 
 
 const G: f32 = 6.67430e-11;
-const METERS_PER_UNIT: f32 = 10.;
 
 #[derive(Clone)]
 pub struct Particle {
+    pub id: u16,
     pub velocity: Vector,
     pub position: Point,
     pub mass: f32,
@@ -15,30 +15,25 @@ pub struct Particle {
 
 impl Particle {
 
-    pub fn new(position_x: f32, position_y: f32, mass: f32) -> Particle {
+    pub fn new(position_x: f32, position_y: f32, mass: f32, id: u16) -> Particle {
         Particle { 
             velocity: Vector::new(0., 0.), 
             position: Point::new(position_x, position_y), 
             mass,
             acceleration: Vector::new(0., 0.),
+            id,
         }
     }
 
     pub fn calculate_acceleration(&mut self, other: &Particle) {
-        let distance = Self::distance(&self.position,&other.position);
-        
-        let force = if distance > 0. {
-            (self.mass * other.mass * G) / (distance * METERS_PER_UNIT).powi(2)
-        } else {
-            0.
-        }.min(1_000_000.);
+        if self.id == other.id { return; }
 
-        self.acceleration.y += (force * (other.position.y - self.position.y)) / self.mass;
-        self.acceleration.x += (force * (other.position.x - self.position.x)) / self.mass;
-    }
+        let distance: Vector = self.position - other.position;
+        let acceleration = (-1. * G * other.mass) / distance.magnitude_squared();
 
-    fn distance(x: &Point, y: &Point) -> f32 {
-        ((y.x - x.x).powi(2) + (y.y - x.y).powi(2)).sqrt()
+        if acceleration.is_nan() { return; } 
+
+        self.acceleration += acceleration * (distance / distance.magnitude());
     }
 }
 
